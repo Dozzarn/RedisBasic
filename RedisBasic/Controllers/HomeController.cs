74b79b90a834e5f95c90274643fdb703936d0ab2
dozzarn <dogu.1769@gmail.com>
2019-01-18 01:13:45 +0300

    stackexchange basic set get

--- RedisBasic/Controllers/HomeController.cs
+++ RedisBasic/Controllers/HomeController.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Caching.Distributed;
 using Newtonsoft.Json;
 using RedisBasic.Models;
+using StackExchange.Redis;
 
 namespace RedisBasic.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IDistributedCache _disturbutedCache;
+        private ConnectionMultiplexer Connection;
         public HomeController(IDistributedCache distributedCache)
         {
+          
             _disturbutedCache = distributedCache;
         }
         
 
         public async Task<IActionResult> Index()
         {
-            Person person = new Person
+           Person person = new Person
             {
                 Name = "Arya",
                 Surname = "Kaşmer",
@@ -44,6 +48,34 @@ namespace RedisBasic.Controllers
             ViewBag.Time = await _disturbutedCache.GetStringAsync(cacheKey);//get string from cache
             var person1 = await _disturbutedCache.GetStringAsync("Person");//get person object data from cache
             var person2 = JsonConvert.DeserializeObject<Person>(person1);// deserialize 
+            return View(person2);
+        }
+
+        [Route("stack")]
+        public async Task<IActionResult> Stack()
+        {
+            Person person = new Person
+            {
+                Name = "Arya",
+                Surname = "Kaşmer",
+                Age = 0,
+                isMail = false
+            };
+
+            Connection = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+            IDatabaseAsync db = Connection.GetDatabase(1);
+            var exp = TimeSpan.FromSeconds(5);
+
+
+            var cacheData = await db.StringGetAsync("Data1");
+            if (string.IsNullOrEmpty(cacheData))
+            {
+                var data = JsonConvert.SerializeObject(person);
+                await db.StringSetAsync("Data1", data, exp);
+
+            }
+            var person1 = await db.StringGetAsync("Data1");
+            var person2 = JsonConvert.DeserializeObject<Person>(person1);
             return View(person2);
         }
     }
